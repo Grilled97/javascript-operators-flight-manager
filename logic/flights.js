aafbb73c003ca92cef065935e634a41b05659e24
Jay Trades <40731985+Grilled97@users.noreply.github.com>
2022-11-29 10:46:52 +1100

    module 1 of unit 2

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,13 @@
 function Flights() {
-
+    function calculateNumberOfFlights(a, b){
+        if (a % b == 0){
+            a / b == c
+        } else if (a % b >= 1){
+            a / b == c++
+        };
+        return c;
+    }
+   return calculateNumberOfFlights()
 }
+module.exports = Flights();
 
