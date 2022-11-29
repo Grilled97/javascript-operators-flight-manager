61fd24731716b83a290515cda70e968f23299d01
Jay Trades <40731985+Grilled97@users.noreply.github.com>
2022-11-29 12:17:42 +1100

    unit 2 module 3

--- logic/flights.js
+++ logic/flights.js
@@ -1,6 +1,10 @@
 function Flights() {
     function calculateNumberOfFlights(a, b){
-        if (a % b == 0){
+        if (a < 0){
+            return "The number of passengers must be a positive integer value"
+        } else if (b < 0){
+            return "The capacity of the flight must be a positive integer value"
+        } else if (a % b == 0){
             let c = a / b;
             return c
         } else if (a % b >= 1){
@@ -8,7 +12,25 @@ function Flights() {
             return ++c
         };
     }
-   return {calculateNumberOfFlights};
+
+   function checkAircraftRevision(distanceLimit, distancesArray){
+    let totalDistance = distancesArray.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+        );
+    if (totalDistance <= (distanceLimit * 0.5)){
+        return "The revision needs to be done within the next 3 months"
+    } else if (totalDistance > (distanceLimit * 0.5) && totalDistance <= (distanceLimit * 0.75 )){
+        return "The revision needs to be done within the next 2 months"
+    } else if (totalDistance > (distanceLimit * 0.75) && totalDistance <= distanceLimit ){
+        return "The revision needs to be done within the next month"
+    } else if (totalDistance > distanceLimit){
+        return "Error"
+    };
+   }
+
+   return {calculateNumberOfFlights, checkAircraftRevision};
    module.exports = Flights();
 };
+
+
 
