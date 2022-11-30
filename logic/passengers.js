9e76bce1d5256b3614eb6db5d90026db5eaf3638
Jaidan Avery <personal@jay-trades.com>
2022-12-01 10:30:18 +1100

    unit 2 module 4 new

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,34 @@
 function Passengers() {
+    checkFlightCapacity(flightCapacity, passengersArray); {
+        let totalPassengers = passengersArray.reduce(
+            (accumulator, currentValue) => accumulator + currentValue
+            );
+        
+        if (totalPassengers > flightCapacity){
+            return "Error"
+        } else if (totalPassengers < flightCapacity); {
+            return totalPassengers;
+        }
+    }
 
+    /* vipPassengers, regularPassengers, 
+    nrOfFlights, businessSeatsPerFlight, economySeatsPerFlight */
+    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers.Array(), 
+        nrOfFlights, businessSeatsPerFlight, economySeatsPerFlight){
+            let vipSeatedBusiness = 0,
+                vipSeatedEconomy = 0,
+                regularSeatedBusiness = 0,
+                regularSeatedEconomy = 0,
+                vipUnseated = 0,
+                economyUnseated = 0;
+
+
+        }
+
+
+
+
+    return {checkFlightCapacity}
+    module.exports = Passengers();
 }
 
