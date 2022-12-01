5f60f175359e617f15457a673802435daef11b12
Jaidan Avery <personal@jay-trades.com>
2022-12-01 11:51:57 +1100

    unit 2 completed check 1

--- logic/passengers.js
+++ logic/passengers.js
@@ -11,24 +11,51 @@ function Passengers() {
         }
     }
 
-    /* vipPassengers, regularPassengers, 
-    nrOfFlights, businessSeatsPerFlight, economySeatsPerFlight */
-    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers.Array(), 
+    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, 
         nrOfFlights, businessSeatsPerFlight, economySeatsPerFlight){
-            let vipSeatedBusiness = 0,
-                vipSeatedEconomy = 0,
+            let vipPassengersWithBusinessSeat = 0,
+                vipPassengersWithEconomySeats = 0,
                 regularSeatedBusiness = 0,
                 regularSeatedEconomy = 0,
                 vipUnseated = 0,
                 economyUnseated = 0;
 
+            for (let passenger of vipPassengers) {
+                if (businessSeatsPerFlight > 0) {
+                    businessSeatsPerFlight--;
+                    vipPassengersWithBusinessSeat++;
+                } else if (economySeatsPerFlight > 0) {
+                    economySeatsPerFlight--;
+                    vipPassengersWithEconomySeats++;
+                } else {
+                    vipUnseated;
+                }
+            }
 
+            for (let passenger of regularPassengers) {
+                if (businessSeatsPerFlight > 0) {
+                    businessSeatsPerFlight--;
+                    regularPassengersWithBusinessSeat++;
+                } else if (economySeatsPerFlight > 0) {
+                    economySeatsPerFlight--;
+                    regularPassengersWithEconomySeats++;
+                } else {
+                    economyUnseated++;
+                }
+            }
+
+            return{
+                vipPassengersWithBusinessSeats: vipPassengersWithBusinessSeat,
+                vipPassengersWithEconomySeats: vipPassengersWithEconomySeats,
+                regularPassengersWithBusinessSeat: regularPassengersWithBusinessSeat,
+                regularPassengersWithEconomySeats: regularPassengersWithEconomySeats
+                }
         }
 
 
 
 
-    return {checkFlightCapacity}
+    return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
     module.exports = Passengers();
 }
 
--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,9 @@
 function Util() {
+    function calculateTotalDistributedPassengers(distributeAllSeatsToAllPassengers){
+        return vipPassengersWithBusinessSeats + vipPassengersWithEconomySeats + regularPassengersWithBusinessSeat + regularPassengersWithEconomySeats
+    }
 
+    return {calculateTotalDistributedPassengers};
+    module.exports = Util();
 }
 
